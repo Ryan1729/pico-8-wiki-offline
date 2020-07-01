e69b0368e3647d929c7392d050a6760f1b25e8ee
Ryan1729 <Ryan1729@gmail.com>
2020-07-01 01:06:00 -0600

    decide that it's too much work to try and get cargo doc to make a site for us and just output html ourselves.

--- src/bin/wiki-dump-to-html/main.rs
+++ src/bin/wiki-dump-to-html/main.rs
@@ -1,5 +1,4 @@
 use std::{
-    collections::BTreeMap,
     env::current_dir,
     io::{BufWriter, Write},
     fs::{
@@ -78,85 +77,46 @@ fn main() -> Res<()> {
         pages.extend(new_pages.into_iter());
     }
 
-    let page_pairs: Vec<_> = pages
-        .into_iter()
-        .map(|page| {
-            let name = munge_to_valid_rust_mod_name(&page.title);
-            (page, name)
-        })
-        .collect();
+    let mut index_path = output_dir.join("index");
+    index_path.set_extension("html");
 
-    let mut page_map: BTreeMap<String, String> = BTreeMap::new();
-
-    for (page, key) in page_pairs.iter() {
-        if page_map.contains_key(key) {
-            return Err(
-                format!(
-                    "pages {} and {} both munge to {}!",
-                    page.title,
-                    page_map.get(key).unwrap(),
-                    &key
-                ).into()
-            );
-        }
-
-        page_map.insert(
-            key.to_owned(),
-            page.title.clone()
-        );
-    }
-
-    let src_dir = confirm_out_dir(output_dir.join("src"))?;
-
-    let mut lib_path = src_dir.join("lib");
-    lib_path.set_extension("rs");
-
-    let lib_file = OpenOptions::new()
+    let index_file = OpenOptions::new()
         .write(true)
         .create(true)
-        .open(lib_path)?;
+        .open(index_path)?;
 
-    let mut lib_writer = BufWriter::new(&lib_file);
+    let mut writer = BufWriter::new(&index_file);
 
-    for (page, mod_name) in page_pairs.iter() {
-        let mut path = src_dir.join(mod_name);
-        path.set_extension("rs");
+    let header = r##"<!DOCTYPE html>
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><style type="text/css">body{
+margin:40px auto;
+max-width:650px;
+line-height:1.6;
+font-size:18px;
+color:#888;
+background-color:#111;
+padding:0 10px
+}
+h1,h2,h3{line-height:1.2}
+a:link {color: #999;}
+a:visited {color: #666;}
+</style></head>
+<body>"##;
 
-        let file = OpenOptions::new()
-            .write(true)
-            .create_new(true)
-            .open(path)?;
+    writeln!(&mut writer, "{}", header)?;
 
-        let mut writer = BufWriter::new(&file);
-        
-        for line in page.text.lines() {
-            writeln!(&mut writer, "//! {}", line)?;
-        }
+    for page in pages.iter() {        
+        writeln!(&mut writer, "<h2>{}</h2>", page.title)?;
 
-        writeln!(&mut lib_writer, "pub mod {};", mod_name)?;
+        writeln!(&mut writer, "<p>{}</p>", page.text)?;
+
+        writeln!(&mut writer, "<hr/>")?;
     }
+    
+    writeln!(&mut writer, "</body></html>")?;
 
     Ok(())
-}
-
-fn munge_to_valid_rust_mod_name(s: &str) -> String {
-    let mut output = s.replace(
-        |c| {
-            !(
-                (c >= 'A' && c <= 'Z')
-                || (c >= 'a' && c <= 'z')
-                || (c >= '0' && c <= '9')
-                || c == '_'
-            )
-        },
-        "_"
-    );
-
-    if output == "_" || output == "lib" {
-        output.push_str("munged")
-    }
-
-    output
 }
 
 fn confirm_out_dir(path: PathBuf) -> Res<PathBuf> {
